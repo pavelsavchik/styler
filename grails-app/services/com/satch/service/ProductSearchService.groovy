ed1a6b403747b412c19c41aa4ff6256282263534
savchik <p.savchik@gmail.com>
2016-02-17 19:33:34 +0300

    Updated ProductSearchService (from old repo).

--- grails-app/services/com/satch/service/ProductSearchService.groovy
+++ grails-app/services/com/satch/service/ProductSearchService.groovy
@@ -19,17 +19,40 @@ class ProductSearchService {
             'classificationGroupId',
             'manufacturer'
     ]
+
+    def PAGING_FIELDS = [
+            'max',
+            'offset'
+    ]
+
+    def SORT_FIELDS = [
+            'price' : 'price.value'
+    ]
+
     def ATTRIBUTE_VALUES_FIELD = 'attributeValues'
 
     List<Product> searchProducts(Map searchParams){
         def query = "from Product pd where 1 = 1 "
         def queryParams = [:]
+        def pagingParams = [:]
 
         SEARCH_FIELDS.each { searchField ->
             if( searchParams[searchField] ) {
                 query += " and $searchField in (:${searchField}List)"
                 queryParams.put("${searchField}List" as String, searchParams[searchField])
             }
+        }
+
+        def minPrice = searchParams.minPrice ? new java.math.BigDecimal(searchParams.minPrice) : null
+        def maxPrice = searchParams.maxPrice ? new java.math.BigDecimal(searchParams.maxPrice) : null
+
+        if(maxPrice){
+            query += " and price.value <= :maxPrice "
+            queryParams.put('maxPrice', maxPrice)
+        }
+        if(minPrice){
+            query += " and price.value >= :minPrice "
+            queryParams.put('minPrice', minPrice)
         }
 
         if(searchParams[ATTRIBUTE_VALUES_FIELD] ){
@@ -40,7 +63,21 @@ class ProductSearchService {
             }
         }
 
-        def products = Product.executeQuery(query, queryParams)
+        def sortField = searchParams['sort']
+        if(sortField && SORT_FIELDS.containsKey(sortField)){
+            query += " order by ${SORT_FIELDS.get(sortField)} "
+            if(searchParams['order'] == 'desc'){
+                query += ' desc '
+            }
+        }
+
+        PAGING_FIELDS.each { pagingField ->
+            if(searchParams[pagingField]){
+                pagingParams << [(pagingField):searchParams[pagingField]]
+            }
+        }
+
+        def products = Product.executeQuery(query, queryParams, pagingParams)
 
         return products
     }
