48c9f1a6d406e36c901827249b72abe39bed959f
savchik <p.savchik@gmail.com>
2016-02-17 19:37:16 +0300

    Migrated ProductController with url mappings (from old repo).

---
+++ grails-app/controllers/com/satch/controllers/ProductController.groovy
@@ -0,0 +1,85 @@
+package com.satch.controllers
+
+import grails.converters.JSON
+import com.satch.domain.Product
+import grails.converters.JSON
+
+//import static org.springframework.http.HttpStatus.BAD_REQUEST
+//import static org.springframework.http.HttpStatus.CREATED
+//import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+//import static org.springframework.http.HttpStatus.NOT_FOUND
+//import static org.springframework.http.HttpStatus.NO_CONTENT
+//import static org.springframework.http.HttpStatus.OK
+
+class ProductController {
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE", show: "GET", list: "GET"]
+
+    def productSearchService
+
+    def list(){
+        def products = productSearchService.searchProducts(params)
+//        product.list()
+
+        response.reset()
+        response.setStatus(206)
+        response.setHeader("Content-range", "products " + params.offset ? params.offset.toString() : "0" + "-" + products.size().toString() + "/" + Long.toString(Product.count(params)));
+        render products as JSON
+    }
+
+    def show(){
+        if(!params.id){
+            return render(status: BAD_REQUEST)
+        }
+
+        def product = Product.get(params.productId)
+        if(!product){
+            return render(status: NOT_FOUND)
+        }
+        render product as JSON
+    }
+
+    def save(){
+        def product = new Product(params)
+        try {
+            product.save(flush: true, failOnError: true)
+        } catch (e) {
+            render(INTERNAL_SERVER_ERROR)
+        }
+        render status: CREATED
+    }
+
+    def update(){
+        if(!params.id){
+            render status: BAD_REQUEST
+        }
+        def product = Product.get(params.id)
+        if(!product){
+            render status: NOT_FOUND
+        } else {
+            bindData(product, params)
+            try {
+                product.save(flush: true, failOnError: true)
+            } catch (e) {
+                render(INTERNAL_SERVER_ERROR)
+            }
+            render status: OK
+        }
+    }
+
+    def delete(){
+        if(!params.id){
+            render status: BAD_REQUEST
+        }
+        def product = Product.get(params.id)
+        if(!product){
+            render status: NOT_FOUND
+        }
+        try {
+            product.delete(flush: true)
+        } catch (e) {
+            render status: INTERNAL_SERVER_ERROR
+        }
+        render status: OK
+    }
+
+}
--- grails-app/controllers/satch/UrlMappings.groovy
+++ grails-app/controllers/satch/UrlMappings.groovy
@@ -12,5 +12,17 @@ class UrlMappings {
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
+
+        "/products"(controller: 'product', action: 'save', method: 'POST')
+        "/products/$catalogId/$productId"(controller: 'product', action: 'update', method: 'PUT')
+        "/products/$catalogId/$productId"(controller: 'product', action: 'delete', method: 'DELETE')
+        "/products/$catalogId/$productId"(controller: 'product', action: 'show', method: 'GET')
+        "/products/$catalogId/$productId/$action/"(controller: 'product', method: 'GET')
+
+        "/products/$id"(controller: 'product', action: 'show', method: 'GET')
+        "/products"(controller: 'product', action: 'save', method: 'POST')
+        "/products"(controller: 'product', action: 'list', method: 'GET')
+        "/products/$id"(controller: 'product', action: 'update', method: 'PUT')
+        "/products/$id"(controller: 'product', action: 'delete', method: 'DELETE')
     }
 }
