96f550d617e6b7caa820381a456bf38834fe1765
savchik <p.savchik@gmail.com>
2016-02-26 21:29:52 +0300

    Modified ProductREST to use id in mapping.

--- grails-app/controllers/com/satch/controllers/ProductController.groovy
+++ grails-app/controllers/com/satch/controllers/ProductController.groovy
@@ -17,8 +17,7 @@ class ProductController {
 
     def productSearchService
 
-
-    def list(){
+    def list() {
         def products = productSearchService.searchProducts(params)
 
         response.reset()
@@ -27,90 +26,89 @@ class ProductController {
         render products as JSON
     }
 
-    def show(){
-        if(!params.productId){
-            return render(status: BAD_REQUEST)
+    def show() {
+        if (!params.id || !params.id.isLong()) {
+            return render(status: HttpURLConnection.HTTP_BAD_REQUEST, text: '')
         }
 
-        def product = Product.findByProductId(params.productId)
-        if(!product){
-            return render(status: NOT_FOUND)
+        def product = Product.get(params.id)
+        if (!product) {
+            return render(status: HttpURLConnection.HTTP_NOT_FOUND, text: '')
         }
-        render product as JSON
+        return render(product as JSON)
     }
 
     @Transactional
-    def save(){
+    def save() {
         Product product = new Product(params)
 
         product.validate()
 
-        if(!product.hasErrors()){
+        if (!product.hasErrors()) {
             try {
                 product.save(flush: true, failOnError: true)
             } catch (e) {
-                response.status = 500
+                response.status = HttpURLConnection.HTTP_INTERNAL_ERROR
                 return render(product.errors.allErrors)
             }
 
-            response.status = 200
+            response.status = HttpURLConnection.HTTP_OK
             return render(product as JSON)
-        }
-        else {
+        } else {
             def errors = product.errors.allErrors.collect() { error ->
                 [field: error.field, message: message(error: error)]
             }
-            response.status = 400
-            return render ([errors: errors] as JSON)
+            response.status = HttpURLConnection.HTTP_BAD_REQUEST
+            return render([errors: errors] as JSON)
         }
     }
 
-    def update(){
-        if(!params.id){
-            render status: BAD_REQUEST
+    @Transactional
+    def update() {
+        if (!params.id || !params.id.isLong()) {
+            return render(status: HttpURLConnection.HTTP_BAD_REQUEST, text: '')
         }
         def product = Product.get(params.id)
-        if(!product){
-            return render(status: NOT_FOUND)
+        if (!product) {
+            return render(status: HttpURLConnection.HTTP_NOT_FOUND, text: '')
         }
 
         bindData(product, params)
         product.validate()
 
-        if(!product.hasErrors()){
+        if (!product.hasErrors()) {
             try {
                 product.save(flush: true, failOnError: true)
             } catch (e) {
-                response.status = 500
+                response.status = HttpURLConnection.HTTP_INTERNAL_ERROR
                 return render(product.errors.allErrors)
             }
 
-            response.status = 200
+            response.status = HttpURLConnection.HTTP_OK
             return render(product as JSON)
-        }
-        else {
+        } else {
             def errors = product.errors.allErrors.collect() { error ->
                 [field: error.field, message: message(error: error)]
             }
             response.status = 400
-            return render ([errors: errors] as JSON)
+            return render([errors: errors] as JSON)
         }
     }
 
-    def delete(){
-        if(!params.id){
-            return render(status: BAD_REQUEST)
+    def delete() {
+        if (!params.id || !params.id.isLong()) {
+            return render(status: HttpURLConnection.HTTP_BAD_REQUEST, text: '')
         }
         def product = Product.get(params.id)
-        if(!product){
-            return render (status: NOT_FOUND)
+        if (!product) {
+            return render(status: HttpURLConnection.HTTP_NOT_FOUND, text: '')
         }
         try {
             product.delete(flush: true)
         } catch (e) {
-            return render (status: INTERNAL_SERVER_ERROR)
+            return render(status: HttpURLConnection.HTTP_INTERNAL_ERROR, text: 'Delete failed.')
         }
-        return render (status: OK)
+        return render(status: HttpURLConnection.HTTP_OK, text: '')
     }
 
 }
--- grails-app/controllers/satch/UrlMappings.groovy
+++ grails-app/controllers/satch/UrlMappings.groovy
@@ -20,11 +20,11 @@ class UrlMappings {
         "/products/$catalogId/$productId"(controller: 'product', action: 'show', method: 'GET')
         "/products/$catalogId/$productId/$action/"(controller: 'product', method: 'GET')
 
-        "/products/$productId"(controller: 'product', action: 'show', method: 'GET')
+        "/products/$id"(controller: 'product', action: 'show', method: 'GET')
         "/products"(controller: 'product', action: 'save', method: 'POST')
         "/products"(controller: 'product', action: 'list', method: 'GET')
-        "/products/$productId"(controller: 'product', action: 'update', method: 'PUT')
-        "/products/$productId"(controller: 'product', action: 'delete', method: 'DELETE')
+        "/products/$id"(controller: 'product', action: 'update', method: 'PUT')
+        "/products/$id"(controller: 'product', action: 'delete', method: 'DELETE')
 
         "/stores/$id"(controller: 'store', action: 'show', method: 'GET')
         "/stores"(controller: 'store', action: 'save', method: 'POST')
