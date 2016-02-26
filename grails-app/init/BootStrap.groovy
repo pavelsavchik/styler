22c27a694f32731d85b4774ca76300bd3c96fae4
savchik <p.savchik@gmail.com>
2016-02-26 18:25:08 +0300

    Changed Product JSON format.

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -10,32 +10,39 @@ class BootStrap {
     def init = { servletContext ->
         //TODO:Find better place for marshaller registration
         JSON.registerObjectMarshaller(Product) { Product product ->
-            if(product) {
+            if (product) {
                 return [
+                        id                 : product.id,
                         productId          : product.productId,
-                        catalog            : product.catalog.catalogId,
-                        classification     : product.classificationId,
-                        classificationGroup: product.classificationGroupId,
+                        catalog            : product.catalog ? [id: product.catalog.id] : null,
+                        classification     : product.classification ? [id: product.classification.id] : null,
+                        classificationGroup: product.classificationGroup ? [id: product.classificationGroup.id] : null,
                         isSearchable       : product.isSearchable,
-                        price              : product.price?.value,
+                        price              : product.price ? [value: product.price.value] : null,
                         manufacturer       : product.manufacturer,
                         longDesc           : product.longDesc,
                         shortDesc          : product.shortDesc,
-                        attributeValues    : product.attributeValues?.collectEntries { attributeValue ->
-                            [(attributeValue.attribute.attributeId): (attributeValue.value)]
+                        attributeValues    : product.attributeValues?.collect { attributeValue ->
+                            [
+                                    attribute: [
+                                            attributeId: attributeValue.attribute.attributeId,
+                                            description: attributeValue.attribute.description
+                                    ],
+                                    value    : attributeValue.value
+                            ]
                         }
                 ]
             }
         }
 
         //create workarea directory if not exists
-        File catalinaBase = new File( System.getProperty( "catalina.base" ) ).getAbsoluteFile()
-        File workareaDir = new File( catalinaBase, "webapps/workarea" )
-        if(!workareaDir.exists()){
+        File catalinaBase = new File(System.getProperty("catalina.base")).getAbsoluteFile()
+        File workareaDir = new File(catalinaBase, "webapps/workarea")
+        if (!workareaDir.exists()) {
             workareaDir.mkdir()
         }
 
-        if(grailsApplication.config.com.satch.populatedb != 'update') {
+        if (grailsApplication.config.com.satch.populatedb != 'update') {
             demoDataInitializationService.initDemoData(50, 5)
         }
     }
